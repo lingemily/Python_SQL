b9d571196970fa4b4e1de909a2f90481ce02cd4c
凌霞芸 <152042718+lingemily@users.noreply.github.com>
2024-03-09 15:53:37 +0800

    Update Lesson6_3.sql

--- SQL/Lesson6_3.sql
+++ SQL/Lesson6_3.sql
@@ -11,3 +11,22 @@ FROM payment
 GROUP BY customer_id
 ORDER BY 總和 DESC;
 
+/*聚合兩標單欄位名稱相同(payment.customer_id = customer.customer_id)*/
+SELECT first_name|| ' ' || last_name AS full_name,
+	SUM(amount) AS 總和
+FROM payment LEFT JOIN customer ON payment.customer_id = customer.customer_id
+GROUP BY full_name
+ORDER BY 總和 DESC;
+
+
+/*使用USING只需要KEY(customer_id)*/
+SELECT first_name|| ' ' || last_name AS full_name,
+	SUM(amount) AS 總和
+FROM payment LEFT JOIN customer USING(customer_id)
+GROUP BY full_name
+ORDER BY 總和 DESC;
+
+
+
+
+
