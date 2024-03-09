c8e2edb1e1f21634667458102daa981868aaee3c
凌霞芸 <152042718+lingemily@users.noreply.github.com>
2024-03-09 10:28:45 +0800

    modify

--- SQL/Lesson5_1.sql
+++ SQL/Lesson5_1.sql
@@ -25,3 +25,7 @@ INSERT INTO 聯絡人(客戶id,聯絡人姓名,電話,郵件)
 VALUES(1,'John Doe','(408)-111-1234','john.doe@bluebird.dev'),
 	  (1,'Jane Doe','(408)-111-1235','jane.doe@bluebird.dev'),
 	  (2,'David Wright','(408)-222-1234','david.wright@dolphin.dev');
+	  
+	  
+DELETE FROM 客戶
+WHERE 客戶_id = 1;
