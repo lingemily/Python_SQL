22f8caa0078a2c9969be374fa52d9cf88f091072
凌霞芸 <152042718+lingemily@users.noreply.github.com>
2024-03-09 15:41:44 +0800

    Update Lesson6_3.sql

--- SQL/Lesson6_3.sql
+++ SQL/Lesson6_3.sql
@@ -2,3 +2,10 @@
 SELECT customer_id,COUNT(*) AS 筆數
 FROM payment
 GROUP BY customer_id;
+
+/* 取出payment的所有客戶應收帳款總和, SUM()*/
+SELECT customer_id,SUM(amount) AS 總和
+FROM payment
+GROUP BY customer_id
+ORDER BY 總和 DESC;
+
