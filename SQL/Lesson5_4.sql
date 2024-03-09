f011a5b0185f22490f6f91113fea987a8651705e
凌霞芸 <152042718+lingemily@users.noreply.github.com>
2024-03-09 11:26:39 +0800

    Update Lesson5_4.sql

--- SQL/Lesson5_4.sql
+++ SQL/Lesson5_4.sql
@@ -29,3 +29,30 @@ SELECT * FROM basket_b;
 /*inner joinL兩個TABLET交集(欄位中的值相同呈現)*/
 SELECT a,fruit_a,b,fruit_b
 FROM basket_a INNER JOIN basket_b ON fruit_a = fruit_b
+
+/*LEFT joinL兩個TABLET交集且左邊TABELE欄位中有的也要呈現*/
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a LEFT JOIN basket_b ON fruit_a = fruit_b
+
+/*LEFT join*/
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a LEFT JOIN basket_b ON fruit_a = fruit_b
+WHERE  b IS NULL
+
+/*right join兩個TABLET交集(欄位中的值相同呈現)**/
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a Right JOIN basket_b ON fruit_a = fruit_b
+
+/*rightjoinL兩個TABLET交集且左邊TABELE欄位中有的也要呈現*/
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a Right JOIN basket_b ON fruit_a = fruit_b
+WHERE a IS NULL
+
+/*full outer join*/
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a FULL OUTER JOIN basket_b ON fruit_a = fruit_b
+
+/* full outer join with where */
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a FULL OUTER JOIN basket_b ON fruit_a = fruit_b	
+WHERE a IS NULL OR b IS NULL
