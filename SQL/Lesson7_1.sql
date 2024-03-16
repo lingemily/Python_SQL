14c479f1fd57dbf505c84cd3ea311fa87ade3af4
凌霞芸 <152042718+lingemily@users.noreply.github.com>
2024-03-16 10:19:54 +0800

    Update Lesson7_1.sql

--- SQL/Lesson7_1.sql
+++ SQL/Lesson7_1.sql
@@ -22,3 +22,38 @@ WHERE 進站人數 = (
 );
 
 
+
+/*各站點進站人數最多的一筆*/
+
+SELECT 站點編號
+FROM gate_count LEFT JOIN stations ON 站點編號 = 編號
+
+
+/*SELECT  MAX(進站人數)
+FROM gate_count
+GROUP BY 站點編號;
+*/
+
+SELECT 站點編號, MAX(進站人數)
+FROM gate_count
+GROUP BY 站點編號;
+
+/*進站人數與站點編號均有重複的,則產生的筆數會有242筆*/
+SELECT *
+FROM gate_count LEFT JOIN stations ON 站點編號 = 編號
+WHERE (站點編號,進站人數) IN(
+	SELECT 站點編號, MAX(進站人數)
+	FROM gate_count
+	GROUP BY 站點編號
+);
+
+
+/*"採用SELECT 站點編號,進站人數,GROUP BY站點編號,進站人數",針對同"站點編號,進站人數"重複的就不會產生的筆數則會有240筆*/
+SELECT 站點編號,進站人數
+FROM gate_count LEFT JOIN stations ON 站點編號 = 編號
+WHERE (站點編號,進站人數) IN(
+	SELECT 站點編號, MAX(進站人數)
+	FROM gate_count
+	GROUP BY 站點編號
+)
+GROUP BY 站點編號,進站人數
